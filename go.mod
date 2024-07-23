17e97432cfcf48cc69efffa72655f4c114878c84
speedphp <jake@speedphp.com>
2024-07-23 15:41:29 +0800

    init mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SpeedPHP/zeroswagger
+
+go 1.22.5
---
+++ pkg/zeroswagger/zeroswagger.go
@@ -0,0 +1,10 @@
+package zeroswagger
+
+import "net/http"
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("X-Middleware", "static-middleware")
+		next(w, r)
+	}
+}
